3c6f925503465da46fc80be4ec49508df80da4d5
ACAPP-dev <acapp909@gmail.com>
2020-07-27 10:06:10 -0700

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,12 @@
 // write your CatList component here
+
+import React from 'react'
+
+export default function CatList(props) {
+    const catReturn = props.catPics.map(cat => {
+        
+        return <img key={cat.id} src={cat.url} alt='cat' width='300' height='300' />
+    })
+    console.log(catReturn)
+    return catReturn
+}
