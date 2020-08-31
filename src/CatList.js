02349ce3b77c5d18b276ad9f3795fb9f6cb8eed3
Alice Freda <alicefreda@gmail.com>
2020-08-31 11:57:10 -0700

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Catlist = ({ catPics }) => {
 
-    const renderCats = catPics.map(catPic => <img key={catPic.id} src={catPic.url} alt={catPic.id} />)
+    const renderCats = catPics.map(catPic => <img key={catPic.id} src={catPic.url} alt={catPic.id}></img>)
 
     return ( 
-        renderCats 
-        )
+        renderCats
+    )
     
 }
 
